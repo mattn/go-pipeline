6320f926a5aa19532176e99130d1d6976f51949b
jiro4989 <jiroron666@gmail.com>
2019-03-23 20:27:30 +0900

    :sparkles: エラー出力も文字列として取得できるようにした

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package pipeline
 import (
 	"fmt"
 	"log"
+	"testing"
 )
 
 func ExampleCommandPipeLine() {
@@ -17,4 +18,28 @@ func ExampleCommandPipeLine() {
 	fmt.Println(string(out))
 	// Output:
 	// 1
+}
+
+func TestCombinedOutput(t *testing.T) {
+	out, err := CombinedOutput(
+		[]string{"echo", "1"},
+		[]string{"grep", "2"},
+	)
+	if err == nil {
+		log.Fatal(err)
+	}
+	if string(out) != "" {
+		log.Fatal("output is not empty.")
+	}
+
+	out, err = CombinedOutput(
+		[]string{"echo", "1"},
+		[]string{"rmdir", "tmptmptmp"},
+	)
+	if err == nil {
+		log.Fatal(err)
+	}
+	if string(out) == "" {
+		log.Fatal("output is empty.")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/jiro4989/go-pipeline
